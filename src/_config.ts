e24e84a2e383c71add1f618b92bf4770f6c0c326
MCMHHM <121157812+MCMHHM@users.noreply.github.com>
2026-09-22 21:48:57 +0800

    更新 _config.ts

--- src/_config.ts
+++ src/_config.ts
@@ -204,7 +204,7 @@ export const db_local_enabled: io = 1;
 export const local_cache_secret = process.env.local_cache_secret || "";
 //PostgreSQL配置(需Read/Write权限)
 import { Pool } from "@neondatabase/serverless"; //导入(不用改)
-export const db_bitio_enabled: io = 0; //启用postgresql数据库
+export const db_bitio_enabled: io = 1; //启用postgresql数据库
 const connectionString = //三种配置方法
   process.env.POSTGRES_URL || //在Vercel项目Storage里连接数据库，并将上方 db_bitio_enabled 设为1
   process.env.db_bitio_pool || //配置环境变量 db_bitio_pool 为下方格式
@@ -233,7 +233,7 @@ export const whitelist_vip_enabled: io = 0;
 
 //===================获取公共黑白名单============================
 //功能开关 1-开 0-关
-export const public_blacklist_enabled: io = 1;
+export const public_blacklist_enabled: io = 0;
 //公共黑名单地址 要求：URL后需可以直接加 UID/mid
 export const public_blacklist: string = "https://black.qimo.ink/api/users/";
 //============================================================
@@ -249,7 +249,7 @@ export const pass_web_on_check: io = 1;
 //限制哔哩漫游最低版本 填写数字 0-不限制
 //1289为1.7.0的版本号,默认限制会定期更新至最新版。
 //1290为目前最新测试版(CI)。
-export const ver_min: number = 1290;
+export const ver_min: number = 0;
 //限制播放特定番剧/视频
 //限制采用"或"策略，满足任意一项即封锁
 //默认屏蔽部分番剧/视频，建议保持
